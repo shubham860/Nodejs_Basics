db8b3401e18d620d6012ba0c45b98776efa5e7e2
shubham860 <shubham.chauhan860@gmail.com>
2021-01-24 00:13:21 +0530

    mongoose middleware

--- Sec7-expressApi-mongoDB(mongoose)/models/tourModels.js
+++ Sec7-expressApi-mongoDB(mongoose)/models/tourModels.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -54,6 +55,11 @@ const tourSchema = new mongoose.Schema({
         default: Date.now(),
     },
     startDates: [Date],
+    slug: String,
+    secretTour : {
+        type: Boolean,
+        default: false
+    }
 },
     {
         toJSON : { virtuals : true },
@@ -64,6 +70,41 @@ const tourSchema = new mongoose.Schema({
 tourSchema.virtual('durationWeeks').get(function (){
     return this.duration / 7 ;
 })
+
+//1(A) DOCUMENT MIDDLEWARE - pre : runs before .save() and .create mongoose methods only it has access to currently processing document
+tourSchema.pre('save',function (next){
+  this.slug  = slugify(this.name, {lower: true})
+    next()
+})
+
+//1(B) DOCUMENT MIDDLEWARE - post : runs after .save() and .create mongoose methods only it give document after save
+tourSchema.post('save',function (doc, next){
+    console.log(doc)
+    next()
+})
+
+
+//2(a) Query middleware -  pre - runs before query is executed on methos find and all types of find and this keyword reference to current query not document
+tourSchema.pre(/^find/, function (next){
+    this.find({secretTour : {$ne : true}}) // .find is method on query
+    this.start = Date.now();
+    next()
+})
+
+//2(a) Query middleware -  post - runs after query is executed on methos find and all types of find and this keyword reference to current query not document
+tourSchema.post(/^find/, function (doc,next){
+    console.log(`Query took ${Date.now() - this.start} ms`)
+    next()
+})
+
+
+// 3(a) Agreegation middleware - pre - run befor any aggregation and this reference to current agggreation document
+tourSchema.pre('aggregate', function (next){
+    this.pipeline().unshift({$match : {secretTour : {$ne : true}}}) // we have to add another match which exclude secret tour true document
+    next()
+})
+
+
 
 const Tour = mongoose.model('Tour', tourSchema);
 
