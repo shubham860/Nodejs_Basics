2bb8ef43bb382fadd82a99a72797c9ed5b4bc8fc
shubham860 <shubham.chauhan860@gmail.com>
2021-02-13 01:10:55 +0530

    update

--- Sec7-expressApi-mongoDB(mongoose)/models/reviewModal.js
+++ Sec7-expressApi-mongoDB(mongoose)/models/reviewModal.js
@@ -35,12 +35,39 @@ const reviewSchema  = new mongoose.Schema({
         toObject : { virtuals : true }
     })
 
+// statics method used on models
+reviewSchema.statics.calcAverageRatings = async function (tourId){
+    const stats = await this.aggregate([
+        {
+            $match : { tour : tourId }
+        },
+        {
+            $group: {
+                _id: '$tour',
+                nRatings: { $sum: 1},
+                avgRating: { $avg: '$rating'}
+            }
+        }
+    ])
+
+    await Tour.findByIdAndUpdate(tourId, {
+        ratingsAverage : stats[0].avgRating,
+        ratingsQuantity : stats[0].nRatings
+    })
+}
+
+// Document middleware for call calcAverageRatings
+reviewSchema.post('save', function (){
+    // this points to current review
+    this.constructor.calcAverageRatings(this.tour);
+})
 
 // query middleware for populating the tour and user in review
 reviewSchema.pre(/^find/, function (next){
     this.populate({path: "tour user", select: "-__v -passwordChangedAt"});
     next();
 })
+
 
 const Review = mongoose.model('review', reviewSchema);
 
